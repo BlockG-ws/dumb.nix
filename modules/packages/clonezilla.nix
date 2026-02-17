a246fc0b3611ba620c50bb4f298555153fdb9ca5
grassblock <i@gb0.dev>
2026-02-17 21:26:35 +0800

    refactor: split clonezilla desktop entry into another file

--- modules/packages/clonezilla.nix
+++ modules/packages/clonezilla.nix
@@ -5,16 +5,5 @@
   ];
 
   # 创建桌面快捷方式
-  environment.etc."skel/.local/share/applications/clonezilla.desktop".text = ''
-    [Desktop Entry]
-    Version=1.0
-    Type=Application
-    Name=Clonezilla
-    Comment=Disk cloning and imaging solution
-    Exec=clonezilla
-    Icon=drive-removable-media
-    Categories=System;Utility;
-    Terminal=true
-    StartupNotify=false
-  '';
+  environment.etc."skel/.local/share/applications/clonezilla.desktop".source = ../../config/clonezilla/clonezilla.desktop ;
 }
