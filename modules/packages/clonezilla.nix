c703ce88feb9cf5a4dcb1d575224a475d6de9044
grassblock <i@gb0.dev>
2026-02-18 13:19:02 +0800

    chore: do not create desktop entry for clonezilla

--- modules/packages/clonezilla.nix
+++ modules/packages/clonezilla.nix
@@ -3,7 +3,4 @@
   environment.systemPackages = with pkgs; [
     clonezilla
   ];
-
-  # 创建桌面快捷方式
-  environment.etc."skel/.local/share/applications/clonezilla.desktop".source = ../../config/clonezilla/clonezilla.desktop ;
 }
